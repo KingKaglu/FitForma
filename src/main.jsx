b8b5f15dc1ecad71004f6cf39f1120ebb7fb8772
KingKaglu <giorgigzirishvili2004l@gmail.com>
2025-06-23 03:50:50 +0400

    შენიშვნა შენი ცვლილების შესახებ

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-// main.jsx ან index.jsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // შეცვალე ეს
 import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> 
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
